a509b9a517080a6e989cedc3ae64844771f9c0ed
Liam Zhang <Liam.Zhang@zoom.us>
2019-03-23 12:37:48 +0800

    vector

--- text.c
+++ text.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <map>
 #include <string>
+#include <vector>
+
 using namespace std;
 
 int main()
@@ -12,16 +14,15 @@ int main()
     map<int, string> mapStudent;
 
     mapStudent.insert(pair<int, string>(1, "student_one"));
-
     mapStudent.insert(pair<int, string>(2, "student_two"));
-
     mapStudent.insert(pair<int, string>(3, "student_three"));
 
     map<int, string>::iterator iter;
 
     for(iter = mapStudent.begin(); iter != mapStudent.end(); iter++)
-
+    {
        cout<<iter->first<<' '<<iter->second<<endl;
+    }
 
     map<int, string>::iterator iter1;
     iter1 = mapStudent.find(1);
@@ -30,6 +31,28 @@ int main()
     else
     {
         mapStudent.insert(pair<int, string>(1, "student_one"));
+    }
+
+    map<int, string>::iterator iter2;
+
+    vector<pair<int, string>>arr;
+    int i;
+
+    for(i = 0; i < 10; i++)
+    {
+        arr.push_back(make_pair(i, "strValue"));
+    }
+
+    for(int j = 0; j < 10; j++)
+    {
+        cout<<arr[j].first<<"      ,     "<<cout<<arr[j].second;
+    }
+
+    vector<pair<int, string>>::iterator iter3;
+
+    for(iter3 = arr.begin(); iter3 != arr.end(); iter3++)
+    {
+        cout<<iter3.first<<"      ,    ,     "<<iter3.second;
     }
 
     return 0;
